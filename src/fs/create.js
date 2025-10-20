7f7315fd237a24d28d9b5bcb00a5517ea930dc49
zagorky <zagorkiynahyi@gmail.com>
2025-10-20 14:48:12 +0300

    feat: implement a "create" task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
+import {access, writeFile} from 'node:fs/promises'
+import {dirname, join} from 'node:path';
+import {fileURLToPath} from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const create = async () => {
-  // Write your code here
+    try {
+        const data = 'I am fresh and young'
+        const filePath = join(__dirname, 'files', 'fresh.txt');
+
+        try {
+            await access(filePath)
+            throw new Error('FS operation failed')
+        } catch (error) {
+            if (error.code !== 'ENOENT') {
+                throw error
+            }
+        }
+
+        await writeFile(filePath, data)
+    } catch (error) {
+        console.log(error.message)
+    }
 };
 
 await create();
