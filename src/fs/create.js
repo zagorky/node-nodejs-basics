7a10e03ef0244c57085128fb504c2c46d71901e0
zagorky <zagorkiynahyi@gmail.com>
2025-10-20 14:51:59 +0300

    refactor: extract file existence check into helper function

--- src/fs/create.js
+++ src/fs/create.js
@@ -4,20 +4,23 @@ import {fileURLToPath} from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+const isFileExists = async (filePath) => {
+    try {
+        await access(filePath)
+        throw new Error('FS operation failed')
+    } catch (error) {
+        if (error.code !== 'ENOENT') {
+            throw error
+        }
+    }
+}
+
 const create = async () => {
     try {
         const data = 'I am fresh and young'
         const filePath = join(__dirname, 'files', 'fresh.txt');
-
-        try {
-            await access(filePath)
-            throw new Error('FS operation failed')
-        } catch (error) {
-            if (error.code !== 'ENOENT') {
-                throw error
-            }
-        }
-
+        await isFileExists(filePath)
         await writeFile(filePath, data)
     } catch (error) {
         console.log(error.message)
