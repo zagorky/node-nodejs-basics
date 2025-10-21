085239429865dc244e5be844633103341d1e5471
zagorky <zagorkiynahyi@gmail.com>
2025-10-21 20:47:59 +0300

    feat: implement rename task with file existence checks and error handling

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,30 @@
+import {getPathData, isFileExists, renameFile, throwError} from "../utils.js";
+import {join} from "node:path";
+import {ERROR_MESSAGES, filesDirectory} from "../constants.js";
+
+
+const properName = 'properFilename.md'
+const wrongName = 'wrongFilename.txt'
+const {dirName} = getPathData(import.meta.url);
+const wrongNamePath = join(dirName, filesDirectory, wrongName);
+const properNamePath = join(dirName, filesDirectory, properName);
+
 const rename = async () => {
-  // Write your code here
+
+    const isSourceFileExists = await isFileExists({path: wrongNamePath})
+    const isTargetFileExists = await isFileExists({path: properNamePath})
+
+    if (!isSourceFileExists || isTargetFileExists) {
+        throwError({
+            message: ERROR_MESSAGES.FS_OPERATION_FAILED,
+            cause: new Error(`Source file doesn't exist or target file already exists`)
+        })
+    }
+
+    renameFile({source: wrongNamePath, destination: properNamePath}).catch((error) => throwError({
+        message: ERROR_MESSAGES.FS_OPERATION_FAILED,
+        cause: error
+    }))
 };
 
 await rename();
