bf47caa397052eca1c8d76787938f58eb3aea48f
zagorky <zagorkiynahyi@gmail.com>
2025-10-21 21:24:31 +0300

    feat: implement read task with file stream and error handling

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import {getPathData, throwError} from "../utils.js";
+import {join} from "node:path";
+import {createReadStream} from "node:fs";
+import {ERROR_MESSAGES, filesDirectory} from "../constants.js";
+
+const {dirName} = getPathData(import.meta.url)
+const fileToReadPath = join(dirName, filesDirectory, 'fileToRead.txt')
+
 const read = async () => {
-  // Write your code here
+    createReadStream(fileToReadPath).pipe(process.stdout).on('error', (error) => throwError({
+        message: ERROR_MESSAGES.FS_OPERATION_FAILED,
+        cause: error
+    }))
 };
 
 await read();
