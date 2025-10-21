5a1e698f6b504cc13831ab3c83b4229c5df3b506
zagorky <zagorkiynahyi@gmail.com>
2025-10-21 21:09:35 +0300

    feat: implement "list" task with file existence check and error handling

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import {getPathData, isFileExists, readDirectory, throwError} from "../utils.js";
+import {join} from "node:path";
+import {ERROR_MESSAGES, filesDirectory} from "../constants.js";
+
+const {dirName} = getPathData(import.meta.url)
+const filesFolderPath = join(dirName, filesDirectory)
+
 const list = async () => {
-  // Write your code here
+
+    isFileExists({path: filesFolderPath}).catch((error) => throwError({
+        message: ERROR_MESSAGES.FS_OPERATION_FAILED,
+        cause: error
+    }))
+
+    readDirectory({directory: filesFolderPath}).then(console.log, (error) => throwError({
+        message: ERROR_MESSAGES.FS_OPERATION_FAILED,
+        cause: error,
+    }))
 };
 
 await list();
