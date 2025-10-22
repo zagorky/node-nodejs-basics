57556443772a1e4be3a382288ca89399e6ba4b6f
zagorky <zagorkiynahyi@gmail.com>
2025-10-22 20:46:23 +0300

    feat: implement argument parsing with logging of key-value pairs

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
+import {logger} from "../utils.js";
+
 const parseArgs = () => {
-  // Write your code here
+    const args = process.argv.slice(2);
+
+    logger({
+        message: Object.entries(args)
+            .filter((_, index) => index % 2 === 0)
+            .map((key, index) => {
+                const propName = key[1].replace('--', '');
+                const value = args[index * 2 + 1];
+                return `${propName} is ${value}`;
+            })
+            .join(', ')
+    })
 };
 
 parseArgs();
