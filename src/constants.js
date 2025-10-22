e4b991e541ff2d44dab9f5d461aa2d43350c7dba
zagorky <zagorkiynahyi@gmail.com>
2025-10-22 21:26:43 +0300

    feat: implement SHA256 hash calculation with error handling and logging

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 export const ERROR_MESSAGES = {
     FS_OPERATION_FAILED: 'FS operation failed',
+    HASH_CALCULATION_FAILED: 'Hash calculation failed'
 }
 
 export const filesDirectory = 'files'
--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import {getPathData, logger, throwError} from "../utils.js";
+import {join} from "node:path";
+import {ERROR_MESSAGES, filesDirectory} from "../constants.js";
+import {createHash} from "node:crypto";
+import {createReadStream} from 'node:fs'
+
+const {dirName} = getPathData(import.meta.url)
+const fileToCalculatePath = join(dirName, filesDirectory, 'fileToCalculateHashFor.txt')
 const calculateHash = async () => {
-  // Write your code here
+    try {
+        const hash = createHash('sha256');
+        const stream = createReadStream(fileToCalculatePath);
+
+        for await (const chunk of stream) {
+            hash.update(chunk);
+        }
+
+        logger({message: `SHA256 hash: ${hash.digest('hex')}`});
+    } catch (error) {
+        throwError({
+            message: ERROR_MESSAGES.HASH_CALCULATION_FAILED,
+            cause: error
+        });
+    }
 };
 
 await calculateHash();
