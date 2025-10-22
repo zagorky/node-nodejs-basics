e3404a9e4218f38af32ccd196a4cabd2f3d74984
zagorky <zagorkiynahyi@gmail.com>
2025-10-22 21:04:56 +0300

    refactor: migrate module to ES modules and update imports/exports

---
+++ src/modules/esm.mjs
@@ -0,0 +1,33 @@
+import {release, version} from 'node:os';
+import {createServer as createServerHttp} from 'node:http';
+import {sep} from 'node:path';
+import FilesA from './files/a.json' with {type: 'json'};
+import FilesB from './files/b.json' with {type: 'json'};
+import {getPathData} from "../utils.js";
+import "./files/c.js";
+
+const random = Math.random();
+
+const {fileName, dirName} = getPathData(import.meta.url)
+
+export const unknownObject = random > 0.5 ? FilesA : FilesB
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${sep}"`);
+
+console.log(`Path to current file is ${fileName}`);
+console.log(`Path to current directory is ${dirName}`);
+
+export const myServer = createServerHttp((_, res) => {
+    res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    console.log('To terminate it, use Ctrl+C combination');
+});
