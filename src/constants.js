80d91e76676727e236c5de80d6fd024faef64661
zagorky <zagorkiynahyi@gmail.com>
2025-10-23 20:56:12 +0300

    feat: implement file compression using Streams API and enhance error constants

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,9 @@ export const ERROR_MESSAGES = {
     STREAM_OPERATION_FAILED: 'Stream operation failed',
     HASH_CALCULATION_FAILED: 'Hash calculation failed',
     SOURCE_FILE_DOESNT_EXIST: "Source file doesn't exist",
-    TARGET_FILE_DOESNT_EXIST: "Target file doesn't exist"
+    TARGET_FILE_DOESNT_EXIST: "Target file doesn't exist",
+    FAIL_TO_COMPRESS: "Error compressing file",
+    FAIL_TO_DECOMPRESS: "Error decompressing file",
 
 }
 
--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,7 +1,23 @@
-// compress.js - implement function that compresses file fileToCompress.txt to archive.gz using zlib and Streams API
+import {createReadStream, createWriteStream,} from 'node:fs';
+import {createGzip} from 'node:zlib';
+import {pipeline} from 'node:stream/promises';
+import {getPathData, throwError} from "../utils.js";
+import {join} from "node:path";
+import {ERROR_MESSAGES, filesDirectory} from "../constants.js";
+
+const {dirName} = getPathData(import.meta.url)
+const inputPath = join(dirName, filesDirectory, 'fileToCompress.txt')
+const outputPath = join(dirName, filesDirectory, 'archive.gz')
+
 const compress = async () => {
-
-
+    try {
+        const gzip = createGzip();
+        const source = createReadStream(inputPath)
+        const destination = createWriteStream(outputPath);
+        await pipeline(source, gzip, destination);
+    } catch (error) {
+        throwError({message: ERROR_MESSAGES.FAIL_TO_COMPRESS, cause: error})
+    }
 };
 
 await compress();
