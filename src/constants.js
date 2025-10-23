24b77cb1b184327bc18b013ce4d575da82eddfa4
zagorky <zagorkiynahyi@gmail.com>
2025-10-23 11:13:02 +0300

    feat: expand error messages with detailed file existence descriptions

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,9 @@
 export const ERROR_MESSAGES = {
     FS_OPERATION_FAILED: 'FS operation failed',
-    HASH_CALCULATION_FAILED: 'Hash calculation failed'
+    HASH_CALCULATION_FAILED: 'Hash calculation failed',
+    SOURCE_FILE_DOESNT_EXIST: "Source file doesn't exist",
+    TARGET_FILE_DOESNT_EXIST: "Target file doesn't exist"
+
 }
 
 export const filesDirectory = 'files'
