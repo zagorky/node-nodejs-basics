5066a30b50d43b0c5bae328455ad25e39804d097
zagorky <zagorkiynahyi@gmail.com>
2025-10-23 12:45:15 +0300

    feat: implement transform task with file streaming and enhanced error handling

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,26 @@
+import {Transform} from "node:stream";
+import {logger, throwError} from "../utils.js";
+import {ERROR_MESSAGES} from "../constants.js";
+
+const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+        this.push(String(chunk).split('').reverse().join(''));
+        callback();
+    }
+})
+
 const transform = async () => {
-  // Write your code here
+    process.stdin
+        .pipe(transformStream)
+        .pipe(process.stdout)
+        .on('error', (error) => throwError({
+            message: ERROR_MESSAGES.STREAM_OPERATION_FAILED,
+            cause: error
+        }))
+        .on('finish', () => {
+            logger({message: 'Completed'});
+        });
+
 };
 
 await transform();
