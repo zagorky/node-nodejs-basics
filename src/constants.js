8c4f558070ae98c9f7be0d02ec96fa56a786dc84
zagorky <zagorkiynahyi@gmail.com>
2025-10-23 12:44:40 +0300

    feat: expand error messages with failed stream message

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 export const ERROR_MESSAGES = {
     FS_OPERATION_FAILED: 'FS operation failed',
+    STREAM_OPERATION_FAILED: 'Stream operation failed',
     HASH_CALCULATION_FAILED: 'Hash calculation failed',
     SOURCE_FILE_DOESNT_EXIST: "Source file doesn't exist",
     TARGET_FILE_DOESNT_EXIST: "Target file doesn't exist"
