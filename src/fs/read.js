55be189ac4ee7d600d9030b22f348c05447ac9e6
zagorky <zagorkiynahyi@gmail.com>
2025-10-23 11:13:21 +0300

    feat: enhance read task with file existence check and switch to promises-based file reading

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,13 +1,21 @@
-import {getPathData, throwError} from "../utils.js";
+import {getPathData, isFileExists, throwError} from "../utils.js";
 import {join} from "node:path";
-import {createReadStream} from "node:fs";
+import {readFile} from 'node:fs/promises'
 import {ERROR_MESSAGES, filesDirectory} from "../constants.js";
 
 const {dirName} = getPathData(import.meta.url)
 const fileToReadPath = join(dirName, filesDirectory, 'fileToRead.txt')
 
 const read = async () => {
-    createReadStream(fileToReadPath).pipe(process.stdout).on('error', (error) => throwError({
+    const isSourceFileExists = await isFileExists({path: fileToReadPath})
+    if (!isSourceFileExists) {
+        throwError({
+            message: ERROR_MESSAGES.FS_OPERATION_FAILED,
+            cause: new Error(`Source file doesn't exist`)
+        })
+    }
+
+    readFile(fileToReadPath, {encoding: 'utf-8'}).then(console.log).catch((error) => throwError({
         message: ERROR_MESSAGES.FS_OPERATION_FAILED,
         cause: error
     }))
