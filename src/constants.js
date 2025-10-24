d583c5fe4989abb56bf0dbdbf93e4abab4792a49
zagorky <zagorkiynahyi@gmail.com>
2025-10-24 13:53:23 +0300

    feat: implement worker thread computation with dynamic worker creation and result logging

--- src/constants.js
+++ src/constants.js
@@ -5,8 +5,7 @@ export const ERROR_MESSAGES = {
     SOURCE_FILE_DOESNT_EXIST: "Source file doesn't exist",
     TARGET_FILE_DOESNT_EXIST: "Target file doesn't exist",
     FAIL_TO_COMPRESS: "Error compressing file",
-    FAIL_TO_DECOMPRESS: "Error decompressing file",
-
+    FAIL_TO_DECOMPRESS: "Error decompressing file"
 }
 
 export const filesDirectory = 'files'
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,14 +1,24 @@
-//main.js - implement function that creates number of worker threads (equal to the number of host machine logical CPU cores)
-// from file worker.js and able to send data to those threads and to receive result of the computation from them.
-// You should send incremental number starting from 10 to each worker. For example: on host machine with 4 cores
-// you should create 4 workers and send 10 to first worker, 11 to second worker, 12 to third worker, 13 to fourth worker.
-// After all workers will finish, function should log array of results into console.
-// The results are array of objects with 2 properties:
-//   status - 'resolved' in case of successfully received value from worker or 'error' in case of error in worker
-//   data - value from worker in case of success or null in case of error in worker
+import {cpus} from 'node:os';
+import {Worker} from 'node:worker_threads';
+import {getPathData, logger} from "../utils.js";
+import {join} from "node:path";
+
+const cpusNumber = cpus().length;
+const {dirName} = getPathData(import.meta.url)
+const workerPath = join(dirName, './worker.js')
+const start = 10
 
 const performCalculations = async () => {
-    // Write your code here
+    const results = await Promise.all(Array.from({length: cpusNumber}, (_, index) => service(start + index)));
+    logger({message: results})
 };
+
+const service = (data) => {
+    return new Promise((resolve) => {
+        const worker = new Worker(workerPath, {workerData: data});
+        worker.on('message', (data) => resolve({status: 'resolved', data}));
+        worker.on('error', () => resolve({status: 'error', data: null}));
+    })
+}
 
 await performCalculations();
