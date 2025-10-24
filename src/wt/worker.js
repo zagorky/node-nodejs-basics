6eb048c2c94ca9a1842d6ffe0f11cba6aecae772
zagorky <zagorkiynahyi@gmail.com>
2025-10-24 13:59:30 +0300

    feat: enhance worker thread to handle nthFibonacci computation with proper error handling

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,11 +1,13 @@
-//worker.js - extend given function to work with data received from main thread
-// and implement function which sends result of the computation to the main thread
-
+import {parentPort, workerData} from 'node:worker_threads';
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    try {
+        parentPort.postMessage({status: 'resolved', data: nthFibonacci(workerData)})
+    } catch {
+        parentPort.postMessage({status: 'error', data: null})
+    }
 };
 
 sendResult();
