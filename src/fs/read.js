6c6a9f6dab146d8395d2b452c7134ad298cd49f5
zagorky <zagorkiynahyi@gmail.com>
2025-10-25 18:36:33 +0300

    refactor: improve logging in fs read operation and enhance error handling

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,4 +1,4 @@
-import {getPathData, throwError} from "../utils.js";
+import {getPathData, logger, throwError} from "../utils.js";
 import {join} from "node:path";
 import {readFile} from 'node:fs/promises'
 import {ERROR_MESSAGES, filesDirectory} from "../constants.js";
@@ -7,10 +7,12 @@ const {dirName} = getPathData(import.meta.url)
 const fileToReadPath = join(dirName, filesDirectory, 'fileToRead.txt')
 
 const read = async () => {
-    readFile(fileToReadPath, {encoding: 'utf-8'}).then(console.log).catch((error) => throwError({
-        message: ERROR_MESSAGES.FS_OPERATION_FAILED,
-        cause: error
-    }))
+    readFile(fileToReadPath, {encoding: 'utf-8'})
+        .then((content) => logger({message: content}),
+            (error) => throwError({
+                message: ERROR_MESSAGES.FS_OPERATION_FAILED,
+                cause: error
+            }))
 };
 
 await read();
