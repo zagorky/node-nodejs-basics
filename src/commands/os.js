46dc2b7f63b4f08865b611b8ca1272114d1850e2
zagorky <zagorkiynahyi@gmail.com>
2025-10-30 20:00:13 +0300

    feat: refactor OS info command handling for improved readability and maintainability

--- src/commands/os.js
+++ src/commands/os.js
@@ -1,34 +1,27 @@
 import {ERROR_MESSAGES} from "../constants.js";
 import {logSuccess} from "../utils.js";
-import {arch, cpus, EOL, homedir, userInfo} from 'node:os';
+import {arch, cpus, EOL, homedir, userInfo} from "node:os";
 
+const commands = {
+    "--EOL": () => logSuccess(JSON.stringify(EOL)),
+    "--cpus": () => {
+        const cpuList = cpus();
+        logSuccess(`Overall amount of CPUs: ${cpuList.length}`);
+        cpuList.forEach((cpu, i) =>
+            logSuccess(`CPU ${i + 1}: ${cpu.model} (${(cpu.speed / 1000).toFixed(2)} GHz)`)
+        );
+    },
+    "--homedir": () => logSuccess(homedir()),
+    "--username": () => logSuccess(userInfo().username),
+    "--architecture": () => logSuccess(arch()),
+};
 
-/**Operating system info (prints following information in console)**/
+/** Operating system info (prints following information in console) */
 export const os = async (args) => {
-    if (!args[0]) {
-        throw new Error(ERROR_MESSAGES.INVALID_INPUT);
-    }
-    switch (args[0]) {
-        case '--EOL':
-            logSuccess(JSON.stringify(EOL));
-            break;
-        case '--cpus':
-            const cpuList = cpus();
-            logSuccess(`Overall amount of CPUS: ${cpuList.length}`);
-            cpuList.forEach((cpu, i) => {
-                logSuccess(`CPU ${i + 1}: ${cpu.model} (${(cpu.speed / 1000).toFixed(2)} GHz)`);
-            });
-            break;
-        case '--homedir':
-            logSuccess(homedir());
-            break;
-        case '--username':
-            logSuccess(userInfo().username);
-            break;
-        case '--architecture':
-            logSuccess(arch());
-            break;
-        default:
-            throw new Error(ERROR_MESSAGES.INVALID_INPUT);
-    }
-}
+    const command = args[0];
+    if (!command) throw new Error(ERROR_MESSAGES.INVALID_INPUT);
+    const action = commands[command];
+    if (!action) throw new Error(ERROR_MESSAGES.INVALID_INPUT);
+
+    action();
+};
