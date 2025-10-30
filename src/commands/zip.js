66713fb9fcc6fb3f6e869a31f24febd981212c4e
zagorky <zagorkiynahyi@gmail.com>
2025-10-30 17:38:59 +0300

    feat: add Brotli compression and decompression functions for file management

---
+++ src/commands/zip.js
@@ -0,0 +1,34 @@
+import {parsePath} from "../utils.js";
+import {createReadStream, createWriteStream,} from 'node:fs';
+import {createBrotliCompress, createBrotliDecompress} from 'node:zlib';
+import {pipeline} from 'node:stream/promises';
+import {ERROR_MESSAGES} from "../constants.js";
+
+/** Compress file (using Brotli algorithm, should be done using Streams API)**/
+export const compress = async (args) => {
+    if (args.length < 2) {
+        throw new Error(ERROR_MESSAGES.INVALID_INPUT);
+    }
+    const srcFile = parsePath(args[0]);
+    const destFile = parsePath(args[1]);
+    await pipeline(
+        createReadStream(srcFile),
+        createBrotliCompress(),
+        createWriteStream(destFile),
+    );
+
+}
+
+
+/** Decompress file (using Brotli algorithm, should be done using Streams API)**/
+export const decompress = async (args) => {
+    if (args.length < 2) {
+        throw new Error(ERROR_MESSAGES.INVALID_INPUT);
+    }
+    const srcFile = parsePath(args[0]);
+    const destFile = parsePath(args[1]);
+    await pipeline(
+        createReadStream(srcFile),
+        createBrotliDecompress(),
+        createWriteStream(destFile));
+}
