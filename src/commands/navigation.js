ebd9260ec8c323e68e7a8ad698a997764dbc26e3
zagorky <zagorkiynahyi@gmail.com>
2025-10-30 17:29:25 +0300

    refactor: remove unnecessary blank line in navigation.js

--- src/commands/navigation.js
+++ src/commands/navigation.js
@@ -29,7 +29,6 @@ export const cd = async (args) => {
     }
 }
 
-
 /**Print in console list of all files and folders in current directory. List should contain:
  *- files and folder names (for files - with extension)
  *- folders and files are sorted in alphabetical order ascending, but list of folders goes first
