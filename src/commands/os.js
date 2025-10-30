aa2484996b4edf0fa72856ac57a275dc8de76266
zagorky <zagorkiynahyi@gmail.com>
2025-10-30 18:22:34 +0300

    feat: add OS information retrieval functions for EOL, CPU, home directory, username, and architecture

---
+++ src/commands/os.js
@@ -0,0 +1,34 @@
+import {ERROR_MESSAGES} from "../constants.js";
+import {logSuccess} from "../utils.js";
+import {arch, cpus, EOL, homedir, userInfo} from 'node:os';
+
+
+/**Get EOL (default system End-Of-Line) and print it to console**/
+export const os = async (args) => {
+    if (!args[0]) {
+        throw new Error(ERROR_MESSAGES.INVALID_INPUT);
+    }
+    switch (args[0]) {
+        case '--EOL':
+            logSuccess(JSON.stringify(EOL));
+            break;
+        case '--cpus':
+            const cpuList = cpus();
+            logSuccess(`Overall amount of CPUS: ${cpuList.length}`);
+            cpuList.forEach((cpu, i) => {
+                logSuccess(`CPU ${i + 1}: ${cpu.model} (${(cpu.speed / 1000).toFixed(2)} GHz)`);
+            });
+            break;
+        case '--homedir':
+            logSuccess(homedir());
+            break;
+        case '--username':
+            logSuccess(userInfo().username);
+            break;
+        case '--architecture':
+            logSuccess(arch());
+            break;
+        default:
+            throw new Error(ERROR_MESSAGES.INVALID_INPUT);
+    }
+}
